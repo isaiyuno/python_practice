2c93880d92fc4c7855bce05c9c3d1a3a5d647c7f
Luo <hktony1@gmail.com>
2023-10-31 16:47:02 -0400

    committing this

---
+++ Exercise_12.py
@@ -0,0 +1,7 @@
+question_about_their_name = input("What is your name? ")
+print(f"Hello {question_about_their_name}, how are you today?")
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weight? ")
+print(f"So, according to you my lord, you are indeed {age} years of age, {height} feet in height, and {weight} in extravagant mass, therefore you are unequivocally bussing my dear lord.")
